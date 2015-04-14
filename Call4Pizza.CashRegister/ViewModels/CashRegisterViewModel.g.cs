928ad760bcfe946ff270eddde8ffc470816f3061
marco.parenzan <marco.parenzan@live.it>
2015-04-14 14:11:23 +0200

    First Commit (without keys)

---
+++ Call4Pizza.CashRegister/ViewModels/CashRegisterViewModel.g.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+using Call4Pizza.CashRegister.ViewModels.Base;
+
+namespace Call4Pizza.CashRegister.ViewModels
+{
+	#region Class CashRegisterViewModel
+
+	public partial class CashRegisterViewModel: BaseViewModel
+    {
+			
+			#region Local Command AddPizza
+			
+			public partial class AddPizzaArgs: BaseArgs
+			{
+			}
+			
+			public AddPizzaArgs AddPizzaCommandArgs
+			{
+				get
+				{
+					AddPizzaArgs args = new AddPizzaArgs {
+					};
+					return args;
+				}
+			}
+
+			partial void OnAddPizza(AddPizzaArgs args);
+
+			partial void OnAddPizzaEnable(Action<bool> enableHandler);
+
+			private RelayCommand<AddPizzaArgs> _addPizza; // ICommand
+
+			public RelayCommand<AddPizzaArgs> AddPizza // ICommand
+			{
+				get
+				{
+					if (_addPizza == null)
+					{
+						_addPizza = new RelayCommand<AddPizzaArgs>(new Action<AddPizzaArgs>(
+							_ =>
+							{
+								try
+								{
+									OnAddPizza(_);
+								}
+								catch(Exception ex)
+								{
+									OnException(ex);
+								}
+							}
+						),
+						_ => {
+							bool enabled = true;
+							OnAddPizzaEnable(enable => {
+								enabled = enable;
+							});
+							return enabled;
+						});
+					}
+					return _addPizza;
+				}
+			}
+			
+			#endregion		
+				
+			#region Local Command AddIngredient
+			
+			public partial class AddIngredientArgs: BaseArgs
+			{
+			}
+			
+			public AddIngredientArgs AddIngredientCommandArgs
+			{
+				get
+				{
+					AddIngredientArgs args = new AddIngredientArgs {
+					};
+					return args;
+				}
+			}
+
+			partial void OnAddIngredient(AddIngredientArgs args);
+
+			partial void OnAddIngredientEnable(Action<bool> enableHandler);
+
+			private RelayCommand<AddIngredientArgs> _addIngredient; // ICommand
+
+			public RelayCommand<AddIngredientArgs> AddIngredient // ICommand
+			{
+				get
+				{
+					if (_addIngredient == null)
+					{
+						_addIngredient = new RelayCommand<AddIngredientArgs>(new Action<AddIngredientArgs>(
+							_ =>
+							{
+								try
+								{
+									OnAddIngredient(_);
+								}
+								catch(Exception ex)
+								{
+									OnException(ex);
+								}
+							}
+						),
+						_ => {
+							bool enabled = true;
+							OnAddIngredientEnable(enable => {
+								enabled = enable;
+							});
+							return enabled;
+						});
+					}
+					return _addIngredient;
+				}
+			}
+			
+			#endregion		
+				
+			#region Local Command SendOrder
+			
+			public partial class CreateOrderArgs: BaseArgs
+			{
+			}
+			
+			public CreateOrderArgs SendOrderCommandArgs
+			{
+				get
+				{
+					CreateOrderArgs args = new CreateOrderArgs {
+					};
+					return args;
+				}
+			}
+
+			partial void OnCreateOrder(CreateOrderArgs args);
+
+			partial void OnSendOrderEnable(Action<bool> enableHandler);
+
+			private RelayCommand<CreateOrderArgs> _sendOrder; // ICommand
+
+			public RelayCommand<CreateOrderArgs> SendOrder // ICommand
+			{
+				get
+				{
+					if (_sendOrder == null)
+					{
+						_sendOrder = new RelayCommand<CreateOrderArgs>(new Action<CreateOrderArgs>(
+							_ =>
+							{
+								try
+								{
+									OnCreateOrder(_);
+								}
+								catch(Exception ex)
+								{
+									OnException(ex);
+								}
+							}
+						),
+						_ => {
+							bool enabled = true;
+							OnSendOrderEnable(enable => {
+								enabled = enable;
+							});
+							return enabled;
+						});
+					}
+					return _sendOrder;
+				}
+			}
+			
+			#endregion		
+			
+		partial void OnException(Exception ex);
+
+					#region Total Property
+			
+			private decimal _total;
+			
+			partial void OnSetTotal(decimal lastTotal, Action<decimal> handleSet);
+			
+			private void SetTotal(decimal alternateValue)
+			{
+				_total = alternateValue;
+			}
+
+			public decimal Total
+			{
+				get
+				{
+					return _total;
+				}
+				
+				set
+				{
+					if (value == _total) return;
+					var lastTotal = _total;
+					_total = value;
+					OnSetTotal(
+						lastTotal
+						, new Action<decimal>(SetTotal));
+					Notify("Total");
+				}
+			}
+			
+			#endregion
+				}
+	
+	#endregion
+}
---
+++ Calll4Pizza.Models/AggregateRoots/QueryingOrder.cs
@@ -0,0 +1,125 @@
+﻿using Call4Pizza.Models.Commands;
+using Call4Pizza.Models.DTO;
+using Call4Pizza.Models.Entities;
+using Call4Pizza.Models.Events;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call4Pizza.Models.AggregateRoots
+{
+    public class QueryingOrder : Call4PizzaAggregateRoot<CommandingOrder, Guid>, IAggregateRootSerializable
+    {
+        protected List<OrderPizza> _pizzas;
+        protected List<OrderBeverage> _beverages;
+
+        protected int _seatings;
+        protected Guid _commandId;
+
+        public QueryingOrder()
+        {
+            _pizzas = new List<OrderPizza>();
+            _beverages = new List<OrderBeverage>();
+        }
+
+        protected override Guid OnGetId()
+        {
+            return _commandId;
+        }
+
+        void IAggregateRootSerializable.SerializeTo(Stream stream)
+        {
+             var serializer = new JsonSerializer();
+             serializer.Converters.Add(new JavaScriptDateTimeConverter());
+             serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (var sw = new StreamWriter(stream))
+            {
+                using (var writer = new JsonTextWriter(sw))
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName("CommandId"); writer.WriteValue(_commandId);
+                    writer.WritePropertyName("Seatings"); writer.WriteValue(_seatings);
+                    writer.WritePropertyName("Pizzas"); serializer.Serialize(writer, _pizzas);
+                    writer.WritePropertyName("Beverages"); serializer.Serialize(writer, _beverages);
+                    writer.WriteEndObject();
+                }
+            }
+        }
+
+        void IAggregateRootSerializable.DeserializeFrom(System.IO.Stream stream)
+        {
+            var serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (var sr = new StreamReader(stream))
+            {
+                using (var reader = new JsonTextReader(sr))
+                {
+                    while(reader.TokenType != JsonToken.StartObject)  reader.Read();
+                    reader.Read();
+                    while (true)
+                    {
+                        if (reader.TokenType == JsonToken.EndObject)
+                        {
+                            reader.Read();
+                            break;
+                        }
+
+                        switch (reader.TokenType)
+                        {
+                            case JsonToken.PropertyName:
+                                DeserializeProperty(serializer, reader, reader.Value as string);
+                                break;
+                            case JsonToken.Comment:
+                                reader.Read();
+                                break;
+                            default:
+                                throw new NotSupportedException("Token not recognized");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DeserializeProperty(JsonSerializer serializer, JsonTextReader reader, string propertyName)
+        {
+            switch (propertyName)
+            {
+                case "CommandId":
+                    _commandId = Guid.Parse(reader.ReadAsString());reader.Read();
+                    break;
+                case "Seatings":
+                    _seatings = reader.ReadAsInt32() ?? 0;reader.Read();
+                    break;
+                case "Pizzas":
+                    reader.Read();
+                    _pizzas = serializer.Deserialize<List<OrderPizza>>(reader);reader.Read();
+                    break;
+                case "Beverages":
+                    reader.Read();
+                    _beverages = serializer.Deserialize<List<OrderBeverage>>(reader);reader.Read();
+                    break;
+                default:
+                    throw new NotSupportedException("Property not recognized");
+            }
+        }
+
+        public IEnumerable<PizzaToDoDTO> PizzasToDo()
+        {
+            return _pizzas.Select(xx => new PizzaToDoDTO { 
+                Description = xx.Description
+                ,
+                Quantity = xx.Quantity
+                ,
+                CommandId = _commandId
+            }).ToList();
+        }
+    }
+}
